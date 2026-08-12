ac5fe93a7c1d026f79d76ee58e21849a91195902
molurudreshsingh-cyber <molurudreshsingh@gmail.com>
2026-08-12 08:35:45 +0000

    datatype-conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let co = false
+
+let numberTovalue = Number(co)
+
+console.log(typeof numberTovalue)
+console.log(numberTovalue)
+
+let bool = 1
+
+let convert = Boolean(bool)
+
+console.log(typeof convert)
+console.log(convert)
+
+let word = "33"
+
+let change = String(word)
+
+console.log(typeof change)
+console.log(change)
