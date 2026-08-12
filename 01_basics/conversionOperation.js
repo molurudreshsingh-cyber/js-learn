65774432a261c6f60946e3cef8f9c27569faa646
molurudreshsingh-cyber <molurudreshsingh@gmail.com>
2026-08-12 10:40:59 +0000

    data_conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,20 +1,50 @@
-let co = false
+// let co = false
 
-let numberTovalue = Number(co)
+// let numberTovalue = Number(co)
 
-console.log(typeof numberTovalue)
-console.log(numberTovalue)
+// console.log(typeof numberTovalue)
+// console.log(numberTovalue)
 
-let bool = 1
+// let bool = 1
 
-let convert = Boolean(bool)
+// let convert = Boolean(bool)
 
-console.log(typeof convert)
-console.log(convert)
+// console.log(typeof convert)
+// console.log(convert)
 
-let word = "33"
+// let word = "33"
 
-let change = String(word)
+// let change = String(word)
 
-console.log(typeof change)
-console.log(change)
+// console.log(typeof change)
+// console.log(change)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3")
+console.log("1" + 2 + 3)
+
+let bool1 = +true
+console.log(bool1);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+console.log(num1)
+console.log(num3)
+console.log(3+4/5*2)
+
+let gameCounter = 100
+
+gameCounter++
+
+let a = 4
+let b = ++a
+//let c = 4++
+//let d = ++5
+let e = a++
+
+console.table([a,b,e,gameCounter])
+
+
+
