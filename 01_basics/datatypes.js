1ecbea178faf9bc4ae55885ca32d30809da522bd
molurudreshsingh-cyber <molurudreshsingh@gmail.com>
2026-08-12 08:00:34 +0000

    Add JavaScript basics

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+let age = 18
+let name = "molu"
+
+
+// data types
+// number = 2 power 53
+// bigint
+// string
+// null = standalone value
+// undefined
+// boolean
+// symbol = unique
+
+console.log(typeof "molu")
+console.log(typeof age)
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+let name = "molu"
+var password = 12345
+const id = 3434
+let email;
+
+console.log(name)
+
+console.log(name, password, id, email)
