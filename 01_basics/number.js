aa10fabad3120507c07e04348b5e330a918df3d7
molurudreshsingh-cyber <molurudreshsingh@gmail.com>
2026-08-12 18:19:07 +0000

    number,  string and math

---
+++ 01_basics/number.js
@@ -0,0 +1,22 @@
+const score = 100
+console.log(score);
+
+const balance = new Number(400)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(8))
+
+const otherNumber = 123.456789
+console.log(otherNumber.toPrecision(3));
+
+let hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Number.MIN_VALUE);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,31 @@
+console.log("word " + "hello")
+
+let word1 = "hello "
+let num = "11"
+
+console.log(word1+num);
+
+console.log(`hi ${word1.toUpperCase()} ${num}`);
+
+const gameName = new String("moluss")
+console.log(gameName[0])
+console.log(gameName.__proto__);
+
+console.log(word1[0]);
+
+console.log(gameName.length);
+console.log(gameName.lastIndexOf);
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('l'));
+
+console.log(gameName.substring(0, 3));
+console.log(gameName.slice(-32, 4));
+
+const anotherString = "   molu   "
+
+console.log(anotherString.trim());
+console.log(anotherString);
+
+
+
+
