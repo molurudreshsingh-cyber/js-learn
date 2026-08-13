747c64590e5917d7ed8fb2e8a8a3a5f3704a8a1b
molurudreshsingh-cyber <molurudreshsingh@gmail.com>
2026-08-13 06:48:27 +0000

    MATH

--- 01_basics/number.js
+++ 01_basics/number.js
@@ -1,21 +1,49 @@
-const score = 100
-console.log(score);
+// Nmber
 
-const balance = new Number(400)
-console.log(balance);
+// const score = 100
+// console.log(score);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(8))
+// const balance = new Number(400)
+// console.log(balance);
 
-const otherNumber = 123.456789
-console.log(otherNumber.toPrecision(3));
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(8))
 
-let hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// const otherNumber = 123.456789
+// console.log(otherNumber.toPrecision(3));
 
-console.log(Number.MIN_VALUE);
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_SAFE_INTEGER);
+// let hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MIN_VALUE);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+//  +++++++++ Maths +++++
+
+console.log(Math.abs(-2.5));
+console.log(Math.round(2.92));
+console.log(Math.ceil(5.1));
+console.log(Math.floor(4.8));
+console.log(Math.min(2,4,5))
+console.log(Math.max(3,4,5));
+
+console.log(Math.random());
+console.log((Math.random()*10));
+console.log((Math.random() *10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+let max = 20
+let min = 10
+
+console.log(Math.floor(Math.random() * (max-min + 1)));
+console.log(Math.floor(Math.random() * (max-min + 1))+min);
+
+
+
+
+
+
 
 
 
